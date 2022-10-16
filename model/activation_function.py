14bd0fa6b473810f381960fb1439482bf5945db6
J Steven Perry <>
2022-10-16 18:24:58 -0500

    Add activation enum

---
+++ model/activation_function.py
@@ -0,0 +1,22 @@
+#
+# The various activation functions that can be added to a layer
+
+from enum import Enum
+
+from model.base_model import BaseModel
+
+
+class Activation(Enum, BaseModel):
+    """
+    Activations. These are the ones currently supported by Keras.
+    """
+    RELU = 10
+    SIGMOID = 20
+    SOFTMAX = 30
+    SOFTPLUS = 40
+    SOFTSIGN = 50
+    TANH = 60
+    SELU = 70
+    ELU = 80
+    EXPONENTIAL = 90
+
--- model/network_descriptor.py
+++ model/network_descriptor.py
@@ -13,11 +13,16 @@ class NetworkDescriptor(BaseModel):
     """
     __slots__ = [
         "network_id", "number_of_inputs", "number_of_outputs", "seasons_trained", "network_hidden_layout",
-        "activation_function", "loss_function", "output_layer_loss_function", "weight_init_function",
-        "updater_function", "normalizer_function", "activation_function", "dropout", "training_error",
-        "network_training_threshold", "hidden_layer_algorithm", "number_of_hidden_layers",
-        "number_of_training_epochs", "when_trained", "accuracy", "precision", "recall", "f1_score",
-        "l2", "network_type"
+        "activation_function", "activation_function_parameters",
+        "loss_function", "loss_function_parameters",
+        "output_layer_loss_function", "output_layer_loss_function_parameters",
+        "weight_init_function", "weight_init_function_parameters",
+        "optimizer_function", "optimizer_function_parameters",
+        "normalizer_function", "normalizer_function_parameters",
+        "output_layer_activation_function", "output_layer_activation_function_parameters",
+        "dropout", "training_error", "network_training_threshold", "hidden_layer_algorithm",
+        "number_of_hidden_layers", "number_of_training_epochs", "when_trained", "accuracy",
+        "precision", "recall", "f1_score", "l2", "network_type"
     ]
 
     def __init__(self, network_id):
